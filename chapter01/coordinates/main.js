c4c4fec1f87ef02cbebb4e17209ab6f8a92d9a74
hakim <the9616@naver.com>
2021-04-04 17:20:37 +0900

    translate

--- chapter01/coordinates/main.js
+++ chapter01/coordinates/main.js
@@ -5,16 +5,21 @@ const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mouseover', (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  horizontal.style.top = `${y}px`;
-  vertical.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
+  document.addEventListener('mouseover', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
 
-  tag.innerHTML = `${x}px, ${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px,
+      ${y - targetHalfHeight}px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
 });
