e04003115e0affc3490f3e0aeb32099b8239bb57
the9616 <the9616@naver.com>
2021-04-08 10:43:36 +0900

    shopping list final

--- chapter02/shopping-list/main.js
+++ chapter02/shopping-list/main.js
@@ -11,12 +11,16 @@ function onAdd() {
     input.focus();
     return;
   }
+
   // 2. 새로운 아이템을 만든다. (텍스트 + 삭제 버튼)
   const item = createItem(text);
+
   // 3. items 컨테이너 안에 새로 만든 아이템을 추가한다.
   items.appendChild(item);
+
   // 4. 새로 추가된 아이템으로 스크롤링
   item.scrollIntoView({ block: 'center' });
+
   // 5. 인풋을 초기화 한다.
   input.value = '';
   input.focus();
@@ -35,26 +39,29 @@ function createItem(text) {
 
   const deleteBtn = document.createElement('button');
   deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  deleteBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
 
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
+  const divider = document.createElement('div');
+  divider.setAttribute('class', 'item__divider');
 
   item.appendChild(name);
   item.appendChild(deleteBtn);
 
   itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.appendChild(divider);
 
   return itemRow;
 }
 
-addBtn.addEventListener('keypress', (event) => {
+addBtn.addEventListener('click', () => {
+  onAdd();
+});
+
+input.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     onAdd();
   }
-});
-
-addBtn.addEventListener('click', () => {
-  onAdd();
 });
